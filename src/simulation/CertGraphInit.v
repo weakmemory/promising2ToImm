df5cf47bd61dfc323d67a8b749e9784f7a631dd2
Anton Podkopaev <podkoav239@gmail.com>
2019-08-27 12:48:33 +0200

    Added CertExecution2.v.

--- src/simulation/CertGraphInit.v
+++ src/simulation/CertGraphInit.v
@@ -10,12 +10,12 @@ From imm Require Import imm_s_hb.
 From imm Require Import imm_s.
 From imm Require Import SubExecution.
 From imm Require Import CertCOhelper.
-From imm Require Import CertExecution2.
 From imm Require Import CombRelations.
 From imm Require Import Prog.
 From imm Require Import Receptiveness.
 From imm Require Import ProgToExecution ProgToExecutionProperties.
 
+Require Import CertExecution2.
 Require Import TraversalConfig.
 Require Import MaxValue.
 Require Import ViewRel.
