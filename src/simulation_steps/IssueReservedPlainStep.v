b9ed6761dc81565ebf7251cb888364c399380de0
Anton Podkopaev <podkoav239@gmail.com>
2019-11-15 08:21:51 +0300

    wip

---
+++ src/simulation_steps/IssueReservedPlainStep.v
@@ -0,0 +1,168 @@
+Require Import PArith Arith.
+From hahn Require Import Hahn.
+Require Import PromisingLib.
+From Promising2 Require Import Configuration TView View Time Event Cell Thread Memory Local.
+From imm Require Import Events.
+From imm Require Import Execution.
+From imm Require Import Execution_eco.
+From imm Require Import imm_s_hb.
+From imm Require Import imm_s.
+From imm Require Import imm_common.
+From imm Require Import CombRelations.
+From imm Require Import CombRelationsMore.
+From imm Require Import ProgToExecution.
+
+Require Import TraversalConfig.
+Require Import ExtTraversal.
+Require Import MaxValue.
+Require Import ViewRel.
+Require Import ViewRelHelpers.
+Require Import SimulationRel.
+Require Import SimulationPlainStepAux.
+Require Import PlainStepBasic.
+Require Import SimState.
+Require Import SimStateHelper.
+Require Import PromiseLTS.
+Require Import MemoryAux.
+Require Import FtoCoherent.
+Require Import AuxRel2.
+Require Import ExistsIssueReservedInterval.
+Require Import IssueReservedStepHelper.
+Require Import MemoryClosedness.
+Require Import SimulationRelProperties.
+
+Set Implicit Arguments.
+
+Section IssueReservedPlainStep.
+
+Variable G : execution.
+Variable WF : Wf G.
+Variable sc : relation actid.
+Variable CON : imm_consistent G sc.
+
+Notation "'E'" := G.(acts_set).
+Notation "'sb'" := G.(sb).
+Notation "'rf'" := G.(rf).
+Notation "'co'" := G.(co).
+Notation "'rmw'" := G.(rmw).
+Notation "'data'" := G.(data).
+Notation "'addr'" := G.(addr).
+Notation "'ctrl'" := G.(ctrl).
+
+Notation "'fr'" := G.(fr).
+Notation "'coe'" := G.(coe).
+Notation "'coi'" := G.(coi).
+Notation "'deps'" := G.(deps).
+Notation "'rfi'" := G.(rfi).
+Notation "'rfe'" := G.(rfe).
+Notation "'detour'" := G.(detour).
+Notation "'hb'" := G.(hb).
+Notation "'sw'" := G.(sw).
+
+Notation "'lab'" := G.(lab).
+
+Notation "'R'" := (fun a => is_true (is_r lab a)).
+Notation "'W'" := (fun a => is_true (is_w lab a)).
+Notation "'F'" := (fun a => is_true (is_f lab a)).
+Notation "'RW'" := (R ∪₁ W).
+Notation "'FR'" := (F ∪₁ R).
+Notation "'FW'" := (F ∪₁ W).
+
+Notation "'Pln'" := (fun a => is_true (is_only_pln lab a)).
+Notation "'Rlx'" := (fun a => is_true (is_rlx lab a)).
+Notation "'Rel'" := (fun a => is_true (is_rel lab a)).
+Notation "'Acq'" := (fun a => is_true (is_acq lab a)).
+Notation "'Acqrel'" := (fun a => is_true (is_acqrel lab a)).
+Notation "'Acq/Rel'" := (fun a => is_true (is_ra lab a)).
+Notation "'Sc'" := (fun a => is_true (is_sc lab a)).
+
+Notation "'Loc_' l" := (fun x => loc lab x = Some l) (at level 1).
+Notation "'W_ex'" := G.(W_ex).
+Notation "'W_ex_acq'" := (W_ex ∩₁ (fun a => is_true (is_xacq lab a))).
+
+Lemma reserve_step PC T S f_to f_from thread w smode
+      (SIMREL_THREAD : simrel_thread G sc PC T S f_to f_from thread smode)
+      (TSTEP : ext_itrav_step
+                 G sc w (mkETC T S)
+                 (mkETC
+                    (mkTC (covered T) (issued T ∪₁ eq w))
+                    (S ∪₁ eq w ∪₁ dom_sb_S_rfrmw G (mkETC T S) rfi (eq w))))
+      (SW : S w)
+      (NONEXT : dom_sb_S_rfrmw G (mkETC T S) rfi (eq w) ⊆₁ ∅)
+      (WTID : thread = tid w) :
+  let T' := mkTC (covered T) (issued T ∪₁ eq w) in
+  let S' := S ∪₁ eq w ∪₁ dom_sb_S_rfrmw G (mkETC T S) rfi (eq w) in
+  exists PC',
+    ⟪ PCSTEP : plain_step MachineEvent.silent thread PC PC' ⟫ /\
+    ⟪ SIMREL_THREAD : simrel_thread G sc PC' T' S' f_to f_from thread smode ⟫ /\
+    ⟪ SIMREL :
+        smode = sim_normal -> simrel G sc PC T S f_to f_from ->
+        simrel G sc PC' T' S' f_to f_from ⟫.
+Proof using WF CON.
+  cdes SIMREL_THREAD. cdes COMMON. cdes LOCAL.
+  subst.
+  
+  assert (tc_coherent G sc T) as TCCOHs by apply TCCOH.
+
+  assert (~ issued T w) as NISSB.
+  { eapply ext_itrav_step_iss_nI with (T:=mkETC T S); eauto. }
+  assert (S ⊆₁ E ∩₁ W) as SEW.
+  { apply set_subset_inter_r. split; [by apply TCCOH|].
+    apply (reservedW WF TCCOH). }
+  assert (E w /\ W w) as [EW WW] by (by apply SEW).
+  (* assert (~ covered T w) as NCOVB. *)
+  (* { intros AA. apply NISSB. eapply w_covered_issued; eauto. by split. } *)
+
+  assert (exists locw, loc lab w = Some locw) as [locw WLOC] by (by apply is_w_loc).
+  assert (exists valw, val lab w = Some valw) as [valw WVAL] by (by apply is_w_val).
+  
+  edestruct issue_reserved_step_helper as [p_rel]; eauto. simpls; desc.
+
+  (* set (pe := *)
+  (*        ThreadEvent.promise *)
+  (*          locw (f_from' w) (f_to' w) Message.reserve *)
+  (*          Memory.op_kind_add). *)
+  
+  (* TODO: continue from here *)
+  eexists.
+  apply and_assoc. apply pair_app; unnw.
+  { split.
+    { set (pe' := MachineEvent.silent).
+      assert (pe' = ThreadEvent.get_machine_event pe) as H.
+      { unfold pe. simpls. }
+      rewrite H. clear H.
+      econstructor; eauto.
+      2: by unfold pe; desf.
+      apply Thread.step_promise.
+      constructor.
+      2: by simpls.
+      econstructor; eauto. }
+    subst.
+    red; splits; red; splits; eauto.
+    { apply TSTEP. }
+    { eapply Memory.add_inhabited; eauto. }
+    { eapply Memory.add_closed; eauto. }
+    simpls.
+    exists state; eexists.
+    rewrite IdentMap.gss.
+    splits; eauto.
+    { eapply sim_tview_f_issued with (f_to:=f_to); eauto. }
+    eapply tview_closedness_preserved_add; eauto. }
+  intros [PCSTEP SIMREL_THREAD']; split; auto.
+  intros SMODE SIMREL.
+  eapply simrel_fS in SIMREL; eauto.
+  eapply full_simrel_step.
+  15: by apply SIMREL.
+  13: { ins. rewrite IdentMap.Facts.add_in_iff.
+        split; auto. intros [|]; auto; subst.
+        apply IdentMap.Facts.in_find_iff.
+          by rewrite LLH. }
+  all: simpls; eauto.
+  8: by eapply msg_preserved_add; eauto.
+  7: by eapply closedness_preserved_add; eauto.
+  { eapply coveredE; eauto. }
+  rewrite issuedE; eauto.
+  all: basic_solver.
+Qed.
+
+End IssueReservedPlainStep.
