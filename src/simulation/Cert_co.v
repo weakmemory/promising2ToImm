4dcf7e9ec3312b42b14c628eb3b060b8a945903a
Ori Lahav <orilahav@gmail.com>
2020-01-13 17:17:48 +0200

    functional (immediate cert_co)

--- src/simulation/Cert_co.v
+++ src/simulation/Cert_co.v
@@ -9,6 +9,11 @@ From imm Require Import imm_s.
 From imm Require Import imm_common_more.
 From imm Require Import CertCOhelper.
 From imm Require Import CombRelations.
+From imm Require Import AuxDef.
+
+
+(* From imm Require Import Events Execution Execution_eco *)
+(*      imm_bob imm_s_ppo imm_s imm_s_hb CombRelations AuxDef. *)
 
 Require Import AuxRel2.
 Require Import TraversalConfig.
@@ -295,6 +300,24 @@ Proof using All.
   all: basic_solver 12.
 Qed.
 
-
+Lemma imm_cert_cof : functional (immediate cert_co).
+Proof using WF S_in_W ST_in_E S IT_new_co.
+  intros x y z ICOXY ICOXZ.
+  assert (cert_co x y) as COXY by apply ICOXY.
+  assert (cert_co x z) as COXZ by apply ICOXZ.
+  apply wf_cert_coD in COXY. destruct_seq COXY as [BB1 BB2].
+  apply wf_cert_coE in COXY. destruct_seq COXY as [BB3 BB4].
+  apply wf_cert_coD in COXZ. destruct_seq COXZ as [AA1 AA2].
+  apply wf_cert_coE in COXZ. destruct_seq COXZ as [AA3 AA4].
+  apply is_w_loc in AA1. desf.
+  set (CC:=COXY). apply wf_cert_col in CC. red in CC.
+  set (DD:=COXZ). apply wf_cert_col in DD. red in DD.
+  destruct (classic (y = z)); auto.
+  edestruct (wf_cert_co_total); eauto.
+  1,2: split; [split|]; eauto.
+  { by etransitivity; [|by eauto]. }
+  { exfalso. by apply ICOXZ with (c:=y). }
+  exfalso. by apply ICOXY with (c:=z).
+Qed.
 
 End CertExec_CO.
