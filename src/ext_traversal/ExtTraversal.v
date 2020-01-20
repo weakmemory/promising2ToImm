5741e9c1b59148091cad8c3198a936e8064b515a
Anton Podkopaev <podkoav239@gmail.com>
2020-01-20 11:01:06 -0600

    Fixed minor admit in ExtTraversal.v

--- src/ext_traversal/ExtTraversal.v
+++ src/ext_traversal/ExtTraversal.v
@@ -565,12 +565,12 @@ Proof using WF IMMCON.
   constructor; unfold dom_sb_S_rfrmw, eissued, ecovered; simpls.
   { by apply init_trav_coherent. }
   { basic_solver. }
-Admitted.
-(*   6: rewrite WF.(rppo_in_sb). *)
-(*   2-6: rewrite no_sb_to_init; basic_solver. *)
-(*   intros x [AA BB]. intuition. *)
-(*   rewrite WF.(W_ex_not_init). basic_solver. *)
-(* Qed. *)
+  6: rewrite WF.(rppo_in_sb).
+  2-6: rewrite no_sb_to_init; basic_solver.
+  { intros x [AA BB]. intuition. }
+  rewrite rmw_W_ex.
+  all: rewrite WF.(W_ex_not_init); basic_solver.
+Qed.
 
 Lemma ext_itrav_stepE e T T' (STEP : ext_itrav_step e T T') : E e.
 Proof using.
