f519fe5cc70c229bec8428f7fffdce02acff31d3
fresheed <ok.john.rus@gmail.com>
2022-05-05 18:52:04 +0200

    Adjusted ExtTraversalConfig to use TLS, changed names and prefixes

--- src/ext_traversal/ExtTraversalConfig.v
+++ src/ext_traversal/ExtTraversalConfig.v
@@ -2,10 +2,13 @@ Require Import Setoid.
 From hahn Require Import Hahn.
 From imm Require Import AuxDef Events Execution Execution_eco
      imm_bob imm_s_ppo imm_s imm_s_hb CombRelations AuxRel2.
-From imm Require Import TraversalConfig Traversal.
+(* From imm Require Import TraversalConfig Traversal. *)
 Require Import AuxRel.
 Require Export ExtTravRelations.
-From imm Require Import TraversalProperties.
+(* From imm Require Import TraversalProperties. *)
+Require Import TlsAux.
+From imm Require Import TraversalOrder. 
+From imm Require Import TLSCoherency. 
 
 Set Implicit Arguments.
 
@@ -85,43 +88,60 @@ Notation "'Acq/Rel'" := (fun a => is_true (is_ra lab a)).
 (** **   *)
 (******************************************************************************)
 
-Record ext_trav_config :=
-  mkETC { etc_TC : trav_config; reserved : actid -> Prop; }.
+(* Record ext_trav_config := *)
+(*   mkETC { etc_TC : trav_config; reserved : actid -> Prop; }. *)
 
-Definition eissued  T := issued  (etc_TC T).
-Definition ecovered T := covered (etc_TC T).
+(* Definition eissued  T := issued  (etc_TC T). *)
+(* Definition ecovered T := covered (etc_TC T). *)
 
-Definition dom_sb_S_rfrmw T rrf P :=
+Definition dom_sb_S_rfrmw (T: trav_label -> Prop) rrf P :=
   dom_rel (sb ⨾ ⦗reserved T⦘) ∩₁ codom_rel (⦗P⦘ ⨾ rrf ⨾ rmw).
 
-Record etc_coherent (T : ext_trav_config) :=
+(* Record etc_coherent (T : ext_trav_config) := *)
+(*   mkETCC { *)
+(*   etc_tccoh          : tc_coherent G sc (etc_TC T); *)
+(*   etc_S_in_E         : reserved T ⊆₁ E; *)
+(*   etc_I_in_S         : eissued T ⊆₁ reserved T; *)
+(*   etc_S_I_in_W_ex    : reserved T \₁ eissued T ⊆₁ W_ex; *)
+(*   etc_F_sb_S         : dom_rel (⦗F∩₁Acq/Rel⦘ ⨾ sb ⨾ ⦗reserved T⦘) ⊆₁ ecovered T ; *)
+(*   etc_dr_R_acq_I     : dom_rel ((detour ∪ rfe) ⨾ (rmw ⨾ rfi)＊ ⨾ ⦗R∩₁Acq⦘ ⨾ sb ⨾ ⦗reserved T⦘) ⊆₁ eissued T ; *)
+(*   etc_W_ex_sb_I      : dom_rel (⦗W_ex_acq⦘ ⨾ sb ⨾ ⦗reserved T⦘) ⊆₁ eissued T ; *)
+(*   etc_sb_S           : dom_sb_S_rfrmw T (rf ⨾ ⦗R_ex⦘) (eissued T) ⊆₁ reserved T; *)
+(*   (* TODO: uncomment the next property for handling FADDs. *) *)
+(*   (* etc_sb_Acq_S       : dom_rel ((rmw ⨾ rfi)＊ ⨾ ⦗R∩₁Acq⦘ ⨾ sb ⨾ ⦗reserved T⦘) ∩₁ codom_rel (⦗eissued T⦘ ⨾ rf ⨾ rmw); *) *)
+(*   etc_rppo_S         : dom_rel ((detour ∪ rfe) ⨾ (data ∪ rfi ∪ rmw)＊ ⨾ rppo ⨾ ⦗ reserved T ⦘) ⊆₁ eissued T; *)
+(*   etc_d_rmw_S        : dom_rel (detour ⨾ rmw ⨾ ⦗ reserved T ⦘) ⊆₁ eissued T; *)
+(*   etc_S_W_ex_rfrmw_I : reserved T ∩₁ W_ex ⊆₁ codom_rel (⦗eissued T⦘ ⨾ rf ⨾ rmw); *)
+(*  }. *)
+Record reserve_coherent (T: trav_label -> Prop) :=
   mkETCC {
-  etc_tccoh          : tc_coherent G sc (etc_TC T);
-  etc_S_in_E         : reserved T ⊆₁ E;
-  etc_I_in_S         : eissued T ⊆₁ reserved T;
-  etc_S_I_in_W_ex    : reserved T \₁ eissued T ⊆₁ W_ex;
-  etc_F_sb_S         : dom_rel (⦗F∩₁Acq/Rel⦘ ⨾ sb ⨾ ⦗reserved T⦘) ⊆₁ ecovered T ;
-  etc_dr_R_acq_I     : dom_rel ((detour ∪ rfe) ⨾ (rmw ⨾ rfi)＊ ⨾ ⦗R∩₁Acq⦘ ⨾ sb ⨾ ⦗reserved T⦘) ⊆₁ eissued T ;
-  etc_W_ex_sb_I      : dom_rel (⦗W_ex_acq⦘ ⨾ sb ⨾ ⦗reserved T⦘) ⊆₁ eissued T ;
-  etc_sb_S           : dom_sb_S_rfrmw T (rf ⨾ ⦗R_ex⦘) (eissued T) ⊆₁ reserved T;
+  rcoh_S_in_E         : reserved T ⊆₁ E;
+  rcoh_I_in_S         : issued T ⊆₁ reserved T;
+  rcoh_S_I_in_W_ex    : reserved T \₁ issued T ⊆₁ W_ex;
+  rcoh_F_sb_S         : dom_rel (⦗F∩₁Acq/Rel⦘ ⨾ sb ⨾ ⦗reserved T⦘) ⊆₁ covered T ;
+  rcoh_dr_R_acq_I     : dom_rel ((detour ∪ rfe) ⨾ (rmw ⨾ rfi)＊ ⨾ ⦗R∩₁Acq⦘ ⨾ sb ⨾ ⦗reserved T⦘) ⊆₁ issued T ;
+  rcoh_W_ex_sb_I      : dom_rel (⦗W_ex_acq⦘ ⨾ sb ⨾ ⦗reserved T⦘) ⊆₁ issued T ;
+  rcoh_sb_S           : dom_sb_S_rfrmw T (rf ⨾ ⦗R_ex⦘) (issued T) ⊆₁ reserved T;
   (* TODO: uncomment the next property for handling FADDs. *)
-  (* etc_sb_Acq_S       : dom_rel ((rmw ⨾ rfi)＊ ⨾ ⦗R∩₁Acq⦘ ⨾ sb ⨾ ⦗reserved T⦘) ∩₁ codom_rel (⦗eissued T⦘ ⨾ rf ⨾ rmw); *)
-  etc_rppo_S         : dom_rel ((detour ∪ rfe) ⨾ (data ∪ rfi ∪ rmw)＊ ⨾ rppo ⨾ ⦗ reserved T ⦘) ⊆₁ eissued T;
-  etc_d_rmw_S        : dom_rel (detour ⨾ rmw ⨾ ⦗ reserved T ⦘) ⊆₁ eissued T;
-  etc_S_W_ex_rfrmw_I : reserved T ∩₁ W_ex ⊆₁ codom_rel (⦗eissued T⦘ ⨾ rf ⨾ rmw);
+  (* rcoh_sb_Acq_S       : dom_rel ((rmw ⨾ rfi)＊ ⨾ ⦗R∩₁Acq⦘ ⨾ sb ⨾ ⦗reserved T⦘) ∩₁ codom_rel (⦗eissued T⦘ ⨾ rf ⨾ rmw); *)
+  rcoh_rppo_S         : dom_rel ((detour ∪ rfe) ⨾ (data ∪ rfi ∪ rmw)＊ ⨾ rppo ⨾ ⦗ reserved T ⦘) ⊆₁ issued T;
+  rcoh_d_rmw_S        : dom_rel (detour ⨾ rmw ⨾ ⦗ reserved T ⦘) ⊆₁ issued T;
+  rcoh_S_W_ex_rfrmw_I : reserved T ∩₁ W_ex ⊆₁ codom_rel (⦗issued T⦘ ⨾ rf ⨾ rmw);
  }.
+  
 
 Section Props.
 
 Variable WF : Wf G.
-Variable T : ext_trav_config.
-Variable ETCCOH : etc_coherent T.
+Variable T : trav_label -> Prop. 
+Variable TCOH : tls_coherent G T.
+Variable RCOH : reserve_coherent T.
 
-Lemma etc_rmw_S : dom_rel ((detour ∪ rfe) ⨾ rmw ⨾ ⦗ reserved T ⦘) ⊆₁ eissued T.
-Proof using WF ETCCOH.
-  rewrite !seq_union_l, dom_union. unionL; [by apply ETCCOH|].
+Lemma rcoh_rmw_S : dom_rel ((detour ∪ rfe) ⨾ rmw ⨾ ⦗ reserved T ⦘) ⊆₁ issued T.
+Proof using WF RCOH.
+  rewrite !seq_union_l, dom_union. unionL; [by apply RCOH|].
   rewrite rmw_W_ex, !seqA. rewrite <- id_inter. rewrite set_interC.
-  rewrite etc_S_W_ex_rfrmw_I; auto. rewrite rfe_in_rf.
+  rewrite rcoh_S_W_ex_rfrmw_I; auto. rewrite rfe_in_rf.
   remember (rf ⨾ rmw) as X.
   arewrite (rf ⨾ rmw ⊆ X) by subst.
   unfolder. ins. desf.
@@ -129,16 +149,16 @@ Proof using WF ETCCOH.
   eapply wf_rfrmwf; eauto.
 Qed.
 
-Lemma eissuedW : eissued T ⊆₁ W.
-Proof using ETCCOH. unfold eissued. eapply issuedW. apply ETCCOH. Qed.
+(* Lemma eissuedW : eissued T ⊆₁ W. *)
+(* Proof using ETCCOH. unfold eissued. eapply issuedW. apply ETCCOH. Qed. *)
 
 Lemma reservedW : reserved T ⊆₁ W.
-Proof using WF ETCCOH.
-  arewrite (reserved T ⊆₁ reserved T \₁ eissued T ∪₁ reserved T ∩₁ eissued T).
-  2: { rewrite eissuedW at 2; auto. rewrite etc_S_I_in_W_ex; auto.
+Proof using WF TCOH RCOH.
+  arewrite (reserved T ⊆₁ reserved T \₁ issued T ∪₁ reserved T ∩₁ issued T).
+  2: { rewrite issuedW at 2; eauto. rewrite rcoh_S_I_in_W_ex; auto.
        rewrite W_ex_in_W; auto. basic_solver. }
   unfolder. ins.
-  destruct (classic (eissued T x)); eauto.
+  destruct (classic (issued T x)); eauto.
 Qed.
 
 End Props.
