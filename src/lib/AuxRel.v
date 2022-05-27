c5ce055568bcc19aa2707685561360d1d549bdfe
Anton Podkopaev <podkoav239@gmail.com>
2022-05-27 10:19:50 +0300

    set_pair_union_{l,r} is moved to imm/

--- src/lib/AuxRel.v
+++ src/lib/AuxRel.v
@@ -1088,24 +1088,3 @@ Proof. red; unfolder; splits; ins; desf; split; eauto. Qed.
 Add Parametric Morphism A : (@set_minus A) with signature 
   set_subset ==> set_subset --> set_subset as set_minus_mori.
 Proof. red; unfolder; splits; ins; desf; eauto. Qed.
-
-Lemma set_pair_union_l {A B} (s s' : A -> Prop) (p : B -> Prop) :
-  (s ∪₁ s') <*> p ≡₁ s <*> p ∪₁ s' <*> p.
-Proof using.
-  clear.
-  unfold set_pair.
-  split. 
-  all: unfolder; ins.
-  all: do 2 desf; eauto.
-Qed.
-
-Lemma set_pair_union_r {A B} (s : A -> Prop) (p p' : B -> Prop) :
-  s <*> (p ∪₁ p') ≡₁ s <*> p ∪₁ s <*> p'.
-Proof using.
-  clear.
-  unfold set_pair.
-  split. 
-  all: unfolder; ins.
-  all: do 2 desf; eauto.
-Qed.
-
