4255bc369d5d6f0dc04849d8a2447cc5018af09b
Anton Podkopaev <podkoav239@gmail.com>
2023-05-31 06:30:02 +0200

    SimulationPlainStep.v, propagation: more progress

--- src/simulation_steps/SimulationPlainStep.v
+++ src/simulation_steps/SimulationPlainStep.v
@@ -419,17 +419,57 @@ Proof using WF CON.
   rewrite set_pair_alt, set_inter_empty_r, set_union_empty_r in ets_upd.
 
   exists PC, f_to, f_from.
+
+  enough (COV : covered  T' ≡₁ covered  T).
+  enough (ISS : issued   T' ≡₁ issued   T).
+  enough (RES : reserved T' ≡₁ reserved T).
+  all: try now rewrite ets_upd; autorewrite with cir_simplify.
+
   assert (simrel_common G sc PC T' f_to f_from smode) as COMMON'.
-  { eapply simrel_common_ta_propagate_irrelevance; eauto.
-    all: rewrite ets_upd.
-    all: now autorewrite with cir_simplify. }
+  { eapply simrel_common_ta_propagate_irrelevance; eauto. }
+
   splits; eauto using rt_refl.
-  { (* TODO: make a lemma *)
-    red. splits; auto.
-    admit. }
+  { red. splits; auto.
+    (* TODO: make a lemma and reuse below *)
+    red in LOCAL. desf.
+    red. do 2 eexists; splits; eauto.
+    6: { eapply sim_state_more.
+         3: rewrite ets_upd; autorewrite with cir_simplify.
+         all: easy. }
+    5: { now rewrite ets_upd; autorewrite with cir_simplify. }
+    { red. ins. apply SIM_PROM0 in PROM. desf.
+      eexists. splits; eauto.
+      { now apply ISS. }
+      intros HH. apply NCOV. now apply COV. }
+    { red. ins. eapply SIM_RPROM0 in RES0. desf.
+      eexists. splits; eauto.
+      { now apply RES. }
+      intros HH. apply NOISS. now apply ISS. }
+    { red. ins. eapply SIM_MEM in VAL; eauto.
+      2: now apply ISS.
+      desf. destruct VAL as [AA BB]. desf. unnw.
+      eexists. splits; eauto.
+      intros CC DD. eapply BB0 in CC.
+      2: { intros HH. apply DD. now apply COV. }
+      desc. splits; auto.
+      eexists. splits; eauto.
+      destruct CC1 as [CC1|CC1]; [left|right].
+      { desc. splits; eauto.
+        intros HH. apply CC1.
+        apply set_subset_single_l. rewrite <- ISS.
+        generalize HH. clear. basic_solver. }
+      desf. exists p. splits; eauto.
+      now apply ISS. }
+    red. ins. apply SIM_RES_MEM0; auto.
+    { now apply RES. }
+    intros HH. apply NISSB. now apply ISS. }
   intros ? HH; subst.
   red. splits; auto.
-  ins.
+  ins. red in HH. desf.
+  (* TODO: make a lemma *)
+  red. apply THREADS in TP. red in TP. desf.
+  do 2 eexists. splits; eauto.
+  (* TODO: reuse from above *)
   admit.
 Admitted. 
 
